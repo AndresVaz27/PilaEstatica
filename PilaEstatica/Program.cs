938d4188d20fc7277705f852283aa5eb262d9a0f
AndresVaz27 <andresvazquezo1@hotmail.com>
2023-10-19 18:48:39 -0600

    ups

--- PilaEstatica/Program.cs
+++ PilaEstatica/Program.cs
@@ -6,85 +6,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        MiPila<int> pilaEnteros = new MiPila<int>(5);
-        pilaEnteros.Push(1);
-        pilaEnteros.Push(2);
-        pilaEnteros.Push(3);
+        const int MaxSize = 5; // Tamaño máximo de la pila
+        int[] stack = new int[MaxSize];
+        int top = -1; // Inicialmente, la pila está vacía
 
-        Console.WriteLine("Elementos en la pila de enteros:");
-        while (pilaEnteros.Count > 0)
+        while (true)
         {
-            Console.WriteLine(pilaEnteros.Pop());
-        }
+            Console.WriteLine("Menú:");
+            Console.WriteLine("1. Push (Insertar elemento)");
+            Console.WriteLine("2. Pop (Eliminar elemento)");
+            Console.WriteLine("3. Mostrar la pila");
+            Console.WriteLine("4. Salir");
+            Console.Write("Selecciona una opción: ");
 
-        MiPila<string> pilaCadenas = new MiPila<string>(3);
-        pilaCadenas.Push("Hola");
-        pilaCadenas.Push("Mundo");
+            int choice = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Elementos en la pila de cadenas:");
-        while (pilaCadenas.Count > 0)
-        {
-            Console.WriteLine(pilaCadenas.Pop());
-        }
-    }
-    public class MiPila<T>
-    {
-        private T[] elementos;
-        private int capacidad;
-        private int contador;
+            switch (choice)
+            {
+                case 1:
+                    if (top < MaxSize - 1)
+                    {
+                        Console.Write("Ingresa un valor para agregar a la pila: ");
+                        int value = int.Parse(Console.ReadLine());
+                        top++;
+                        stack[top] = value;
+                        Console.WriteLine($"Elemento {value} agregado a la pila.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("La pila está llena. No se puede agregar más elementos.");
+                    }
+                    break;
 
-        public MiPila(int capacidad)
-        {
-            this.capacidad = capacidad;
-            this.elementos = new T[capacidad];
-            this.contador = 0;
-        }
+                case 2:
+                    if (top >= 0)
+                    {
+                        int removedItem = stack[top];
+                        top--;
+                        Console.WriteLine($"Elemento {removedItem} eliminado de la pila.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("La pila está vacía. No se puede eliminar ningún elemento.");
+                    }
+                    break;
 
-        public void Push(T elemento)
-        {
-            if (contador < capacidad)
-            {
-                elementos[contador] = elemento;
-                contador++;
-            }
-            else
-            {
-                Console.WriteLine("La pila está llena. No se puede agregar más elementos.");
-            }
-        }
+                case 3:
+                    Console.WriteLine("Elementos en la pila:");
+                    for (int i = top; i >= 0; i--)
+                    {
+                        Console.WriteLine(stack[i]);
+                    }
+                    break;
 
-        public T Pop()
-        {
-            if (contador > 0)
-            {
-                contador--;
-                T elemento = elementos[contador];
-                elementos[contador] = default(T); // Restablecer el valor a su valor predeterminado
-                return elemento;
-            }
-            else
-            {
-                Console.WriteLine("La pila está vacía. No se pueden sacar más elementos.");
-                return default(T); // Valor predeterminado para el tipo T (por ejemplo, null para referencias)
-            }
-        }
+                case 4:
+                    Environment.Exit(0);
+                    break;
 
-        public T Peek()
-        {
-            if (contador > 0)
-            {
-                return elementos[contador - 1];
-            }
-            else
-            {
-                Console.WriteLine("La pila está vacía. No hay elementos para ver.");
-                return default(T); // Valor predeterminado para el tipo T
+                default:
+                    Console.WriteLine("Opción no válida. Por favor, selecciona una opción válida.");
+                    break;
             }
         }
-
-        public int Count
-        {
-            get { return contador; }
-        }
-    }
+    }    
 }
